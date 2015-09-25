e18e66c214f5dcc7fc99acb532628a47888a92ea
artworkjpm <johnnymoran44@hotmail.com>
2015-09-25 11:14:51 +0100

    animated plane

--- assets/js/dancing.js
+++ assets/js/dancing.js
@@ -1,7 +1,56 @@
+var $fly = $('.fly');
 $(document).ready(function () {
-  setInterval(function(){
+  setInterval(function () {
+    $(".dance").effect("bounce", {
+      distance: 20
+    });
+  }, 1500);
+
+  setInterval(function () {
+    $(".slide-in").effect("slide", {
+      distance: 200,
+      direction: "left"
+    });
+  }, 1500);
+
+  setInterval(function () {
+    $(".highlight").effect("highlight", {
+      color: 'yellow'
+    }, 2000);
+  }, 1000);
+
+  //fly
   
-  $(".dance").effect("bounce", { distance: 10 });
-}, 800);
   
-});
+
+    var flyplane = function(){
+      setInterval(function() {
+        $fly.animate({
+            right: '-=50',
+            bottom: '+=50'
+        }, 2000, function() {
+            $fly.removeAttr("style");
+        });
+    }, 2000);
+ 
+};
+  
+  flyplane();
+  
+
+  $(".toggle-effects").on("click", function () {
+    $(this).text(function (i, text) {
+      return text === "Turn off effects" ? "Turn on effects" : "Turn off effects";
+    })
+
+    $("#spin").toggleClass("spin");
+    $("#dance").toggleClass("dance");
+    $("#slide-in").toggleClass("slide-in");
+    $("#film").toggleClass("highlight");
+   
+
+  }); //end of button click
+
+
+
+}); // end of ready function
--- page-hobbies.php
+++ page-hobbies.php
@@ -6,10 +6,14 @@
         <div class="main-container-right clearfix">
 
           <section>
+            
+            <!--<h2 class="page-title">Hobbies</h2>-->
+            <button type="button" class="btn toggle-effects">Turn off effects</button>
+            <div class="hobbie-container">
+            
             <div class="hobbie-box">
-
               <div class="circle-image">
-                <img src="http://localhost/jpmwordpress/wp-content/uploads/2015/09/fa-futbol.png" class="spin-off">
+                <img src="http://localhost/jpmwordpress/wp-content/uploads/2015/09/fa-futbol.png" id="spin" class="spin">
               </div>
                <div class="circle-text">
                 <p>Big football fan, sometimes I play...</p>
@@ -17,17 +21,53 @@
             </div>
             
             <div class="hobbie-box">
-
               <div class="circle-image">
-                <i class="fa fa-music dance-off"></i>
+                <i id="dance" class="fa fa-music dance"></i>
               </div>
                <div class="circle-text">
                 <p>I love music, I have played in punk, indie bands...</p>
               </div>
             </div>
+              
+              <div class="hobbie-box extra-wide">
+              <div class="circle-image">
+                <i id="slide-in" class="fa fa-bicycle slide-in"></i>
+              </div>
+               <div class="circle-text">
+                <p>I cycle to work everyday, I also cycle on weekends:</p>
+                 <ul class="cycle">
+                  <li>My <a href="http://artworkjpm.biz/cycling">cycleblog</a></li>
+                  <li>My <a href="#">Strava</a> profile</li>
+                 </ul>
+              </div>
+            </div>
+              
+              <div class="hobbie-box">
+              <div class="circle-image">
+                <i id="film" class="fa fa-film highlight"></i>
+              </div>
+               <div class="circle-text">
+                <p>Fan of film and tv, Amazon Prime and Netflix subscriber</p>
+                
+              </div>
+            </div>
+              
+              <div class="hobbie-box">
+              <div class="circle-image">
+                <i id="travel" class="fa fa-plane fly"></i>
+                <i id="travel-still" class="fa fa-plane" style="display:none"></i>
+
+              </div>
+               <div class="circle-text">
+                <p>I love travelling and working in the travel industry</p>
+                
+              </div>
+            </div>
+              
+              
             
           
-              
+              </div>
             
           </section>
 
